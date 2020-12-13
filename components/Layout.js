7b88fab507a29e17743576c288b5b3f200224c33
Augustus Li <AugustusLZJ@gmail.com>
2020-12-13 00:15:22 -0500

    Added _document.js, a nextjs official solution, to fixed broken Navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
+import { ThemeProvider } from '@material-ui/styles'
+import theme from "../src/theme";
 import Navbar from "./Navbar"
 
 const Layout = props => (
-  <>
+  <ThemeProvider theme={theme}>
     <Navbar withSearch={props.withSearch} />
     {props.children}
-  </>
+  </ThemeProvider>
 )
 
 export default Layout
